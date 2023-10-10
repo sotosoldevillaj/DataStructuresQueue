5d1faa84dd8103ec46f5247fb7b2d40e4191ad3b
lauzeb <116819717+lauzeb@users.noreply.github.com>
2023-10-10 09:34:15 -0400

    Add Files

---
+++ StoreSimulation.java
@@ -0,0 +1,16 @@
+
+package hw3;
+
+
+/**
+ * 
+ * 
+ * @author Benjamin Lauze
+ * @version 1.0.0 2023-10-10 Initial implementation
+ *
+ */
+public class StoreSimulation
+    {
+
+    }
+   // end class StoreSimulation
